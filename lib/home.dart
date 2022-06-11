589216ad3796a81b80d10d51de73e0421ee6c765
Ersan Tzampaz Chatip <hatipersan@gmail.com>
2022-06-11 22:04:01 +0300

    Added custom window frame, made the window moveable from the frame, added minimize, resize and close buttons. Made so the windows window frame doesn't show.

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: non_constant_identifier_names
 
+import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:flutter/material.dart';
 import 'package:story_creator/FileFolder/directory_getter.dart';
 
@@ -26,73 +27,121 @@ class _HomeState extends State<Home> {
     "Paste",
   ];
 
+  final buttonColors = WindowButtonColors(
+      iconNormal: Colors.white,
+      mouseOver: Colors.blueGrey[700],
+      mouseDown: Colors.blueGrey[800],
+      iconMouseOver: Colors.white,
+      iconMouseDown: Colors.white);
+
+  final closeButtonColors = WindowButtonColors(
+      mouseOver: const Color(0xFFD32F2F),
+      mouseDown: const Color(0xFFB71C1C),
+      iconNormal: Colors.white,
+      iconMouseOver: Colors.white);
+
+  void maximizeOrRestore() {
+    setState(() {
+      appWindow.maximizeOrRestore();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blueGrey[600],
       appBar: AppBar(
-          leadingWidth: 250,
+          leadingWidth: 120,
           backgroundColor: Colors.blueGrey[900],
           toolbarHeight: 30,
           elevation: 0,
-          title: const SizedBox(
-            width: 0,
-            height: 0,
-          ),
+          title: WindowTitleBarBox(child: MoveWindow()),
+          actions: [
+            MinimizeWindowButton(colors: buttonColors),
+            appWindow.isMaximized
+                ? RestoreWindowButton(
+                    colors: buttonColors,
+                    onPressed: maximizeOrRestore,
+                  )
+                : MaximizeWindowButton(
+                    colors: buttonColors,
+                    onPressed: maximizeOrRestore,
+                  ),
+            CloseWindowButton(colors: closeButtonColors),
+          ],
           leading: Row(children: [
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                PopupMenuButton(
-                    // TODO: add functionality to the buttons
-                    onSelected: ((value) {}),
-                    iconSize: 20,
-                    tooltip: "Files",
-                    color: Colors.blueGrey[900],
-                    position: PopupMenuPosition.under,
-                    icon: const Icon(Icons.insert_drive_file_sharp),
-                    padding: EdgeInsets.zero,
-                    itemBuilder: (context) {
-                      return [
-                        for (int i = 0; i < FilesNavigation.length; i++)
-                          PopupMenuItem(
-                              value: FilesNavigation[i],
-                              child: Text(
-                                FilesNavigation[i],
-                                style: const TextStyle(color: Colors.white),
-                              ))
-                      ];
-                    }),
-                PopupMenuButton(
-                    // TODO: add functionality to the buttons
-                    onSelected: ((value) {}),
-                    iconSize: 20,
-                    tooltip: "Edit",
-                    color: Colors.blueGrey[900],
-                    position: PopupMenuPosition.under,
-                    icon: const Icon(Icons.edit),
-                    padding: EdgeInsets.zero,
-                    itemBuilder: (context) {
-                      return [
-                        for (int i = 0; i < Edit.length; i++)
-                          PopupMenuItem(
-                              value: Edit[i],
-                              child: Text(
-                                Edit[i],
-                                style: const TextStyle(color: Colors.white),
-                              ))
-                      ];
-                    }),
-                    // TODO: add functionality to the button
-                IconButton(
-                    tooltip: "Run",
-                    padding: EdgeInsets.zero,
-                    alignment: Alignment.center,
-                    onPressed: () {},
-                    icon: const Icon(
-                      Icons.play_arrow_rounded,
-                      size: 28,
-                    ))
+                Theme(
+                  data: Theme.of(context).copyWith(
+                    highlightColor: Colors.transparent,
+                    splashColor: Colors.transparent,
+                  ),
+                  child: PopupMenuButton(
+                      // TODO: add functionality to the buttons
+                      onSelected: ((value) {}),
+                      iconSize: 20,
+                      tooltip: "Files",
+                      color: Colors.blueGrey[900],
+                      position: PopupMenuPosition.under,
+                      icon: const Icon(Icons.insert_drive_file_sharp,color: Colors.white,),
+                      padding: EdgeInsets.zero,
+                      itemBuilder: (context) {
+                        return [
+                          for (int i = 0; i < FilesNavigation.length; i++)
+                            PopupMenuItem(
+                                value: FilesNavigation[i],
+                                child: Text(
+                                  FilesNavigation[i],
+                                  style: const TextStyle(color: Colors.white),
+                                ))
+                        ];
+                      }),
+                ),
+                Theme(
+                  data: Theme.of(context).copyWith(
+                    highlightColor: Colors.transparent,
+                    splashColor: Colors.transparent,
+                  ),
+                  child: PopupMenuButton(
+                      // TODO: add functionality to the buttons
+                      onSelected: ((value) {}),
+                      iconSize: 20,
+                      tooltip: "Edit",
+                      color: Colors.blueGrey[900],
+                      position: PopupMenuPosition.under,
+                      icon: const Icon(Icons.edit,color: Colors.white,),
+                      padding: EdgeInsets.zero,
+                      itemBuilder: (context) {
+                        return [
+                          for (int i = 0; i < Edit.length; i++)
+                            PopupMenuItem(
+                                value: Edit[i],
+                                child: Text(
+                                  Edit[i],
+                                  style: const TextStyle(color: Colors.white),
+                                ))
+                        ];
+                      }),
+                ),
+                // TODO: add functionality to the button
+                Theme(
+                  data: Theme.of(context).copyWith(
+                    highlightColor: Colors.transparent,
+                    splashColor: Colors.transparent,
+                  ),
+                  child: IconButton(
+                      tooltip: "Run",
+                      padding: EdgeInsets.zero,
+                      alignment: Alignment.center,
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.play_arrow_rounded,
+                        size: 28,
+                        color: Colors.white,
+                      )),
+                )
               ],
             ),
           ])),
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
 import 'Home.dart';
+import 'package:bitsdojo_window/bitsdojo_window.dart';
 
 void main(List<String> args) {
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       initialRoute: '/home',
       routes: {
         '/home': (context) => Home(),
       },
     )
   );
+  doWhenWindowReady((){
+    final win = appWindow;
+    win.minSize = Size(600,480);
+    win.alignment = Alignment.center;
+    win.title = "RBT Visual Novel Maker";
+  });
 }
