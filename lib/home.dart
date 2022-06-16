8d1649f86f9063d750501fd56f7949c29e655921
Ersan Tzampaz Chatip <hatipersan@gmail.com>
2022-06-16 22:46:07 +0300

    slight rename of a to be implemented widget

--- lib/home.dart
+++ lib/home.dart
@@ -58,7 +58,7 @@ class _HomeState extends State<Home> {
         children: [
           FilesAndDirectories(),
           // TODO: implement StorylineWindow
-          //StorylineWindow(),
+          //StorylineTabs(),
           // TODO: implement Inspector for branches
           //Inspector(),
         ],
